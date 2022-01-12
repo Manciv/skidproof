4fb81296b0bddbbcfb7b46c013203bf747a7a8e2
Water-Boi <91918720+Water-Boi@users.noreply.github.com>
2022-01-11 20:29:46 -0500

    SkidProof 1.0

---
+++ Skidproof.js
@@ -0,0 +1,10 @@
+ <script>
+document.addEventListener("keydown", function (event) {
+    if (event.ctrlKey) {
+        event.preventDefault();
+    }   
+});
+</script>
+    <script>
+        document.addEventListener('contextmenu', event => event.preventDefault());
+        </script>
